c6005f7eaff3a7c7d741a7ca0100ea57b1cf2f19
Peter Golov <petya-golov@mail.ru>
2023-02-21 19:40:06 +0300

    (refactor): deleted useless code

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,26 +1,15 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"1password_copy_project/cmd/service/configure_router"
 	"github.com/gofiber/fiber/v2"
 	_ "github.com/jackc/pgx/v5/stdlib"
-	"log"
-	"os"
 )
 
 func main() {
-	//db, err := sqlx.Connect("pgx", os.Getenv("POSTGRESQL_URL"))
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
-
-	//db.SetMaxOpenConns(10)
-	//db.SetMaxIdleConns(5)
-	//db.SetConnMaxLifetime(5 * time.Minute)
-	//db.SetConnMaxIdleTime(5 * time.Minute)
-
-	//defer db.Close()
-
 	app := fiber.New()
 	configure_router.ConfigureRouter(app)
 
